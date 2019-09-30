b7f329f994fd48a12523a316369cec66daf27fc1
fernandofilho <fernandofilho1310@gmail.com>
2019-09-30 14:21:46 -0300

    Projeto - Rifa App

--- app/src/main/java/com/example/apprifa/Adapters/Adapter_Data_Cobranca.java
+++ app/src/main/java/com/example/apprifa/Adapters/Adapter_Data_Cobranca.java
@@ -5,16 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.apprifa.Controlers.DatasVendasCobranca;
+import com.example.apprifa.Models.DataCobrancaVenda;
 import com.example.apprifa.R;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
 
-public class Adapter_Data_Cobranca extends FirestoreRecyclerAdapter<DatasVendasCobranca, Adapter_Data_Cobranca.ViewHolder_datas> {
+public class Adapter_Data_Cobranca extends FirestoreRecyclerAdapter<DataCobrancaVenda, Adapter_Data_Cobranca.ViewHolder_datas> {
+
+    private OnItemClickListener listener;
 
     /**
      * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
@@ -22,7 +27,7 @@ public class Adapter_Data_Cobranca extends FirestoreRecyclerAdapter<DatasVendasC
      *
      * @param options
      */
-    public Adapter_Data_Cobranca(@NonNull FirestoreRecyclerOptions<DatasVendasCobranca> options) {
+    public Adapter_Data_Cobranca(@NonNull FirestoreRecyclerOptions<DataCobrancaVenda> options) {
         super(options);
     }
 
@@ -30,22 +35,49 @@ public class Adapter_Data_Cobranca extends FirestoreRecyclerAdapter<DatasVendasC
     @Override
     public ViewHolder_datas onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.mostra_dados_datas,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.mostra_dados_datas, parent, false);
 
         return new ViewHolder_datas(view);
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull ViewHolder_datas viewHolder_datas, int i, @NonNull DatasVendasCobranca datasVendasCobranca) {
+    protected void onBindViewHolder(@NonNull ViewHolder_datas viewHolder_datas, int i, @NonNull DataCobrancaVenda datasVendasCobranca) {
 
-
+        viewHolder_datas.datavenda.setText(datasVendasCobranca.getData_venda());
+        viewHolder_datas.datacobranca.setText(datasVendasCobranca.getData_cobranca());
 
     }
 
-    public class ViewHolder_datas extends RecyclerView.ViewHolder{
+    public class ViewHolder_datas extends RecyclerView.ViewHolder {
+
+        TextView datacobranca, datavenda;
 
         public ViewHolder_datas(@NonNull View itemView) {
             super(itemView);
+
+            datacobranca = itemView.findViewById(R.id.txt_data_cobranca);
+            datavenda = itemView.findViewById(R.id.txt_data_venda);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION && listener != null) {
+
+                        listener.onItemClick(getSnapshots().getSnapshot(position), position);
+                    }
+                }
+            });
         }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(DocumentSnapshot documentSnapshot, int position);
+
+    }
+
+    public void setOnItemClicklistener(OnItemClickListener listener) {
+        this.listener = listener;
     }
 }
